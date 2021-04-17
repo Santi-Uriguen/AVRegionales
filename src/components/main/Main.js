9b266f8ca468fca4f1ee1cea52071e7bf2619e89
SantiUriguen <santiuriguen@hotmail.com>
2021-04-17 18:55:00 -0300

    whatsapp

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,12 @@
 import CardContainer from "./CardContainer/CardContainer.js";
 import ContactForm from "./Form/Form.js";
+import Whatsapp from "./Whatsapp/Whatsapp.js";
 export default function Main() {
   return (
     <main className="main">
       <CardContainer />
       <ContactForm />
+      <Whatsapp />
     </main>
   );
 }
---
+++ src/components/main/Whatsapp/Whatsapp.js
@@ -0,0 +1,10 @@
+import whatsapp from "../../../assets/whatsapp.svg";
+export default function Whatsapp() {
+  return (
+    <div className="whatsapp">
+      <a href="https://wa.me/5492615431092/?">
+        <img src={whatsapp} alt="" className="wpIcon" />
+      </a>
+    </div>
+  );
+}
