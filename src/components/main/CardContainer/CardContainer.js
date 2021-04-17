fc31d8638da1411d9c5e0ddbbd07e5a8db2e4b62
SantiUriguen <santiuriguen@hotmail.com>
2021-04-17 11:17:13 -0300

    fixed banners

--- src/components/main/CardContainer/CardContainer.js
+++ src/components/main/CardContainer/CardContainer.js
@@ -1,14 +1,14 @@
 import CardTemplate from "./components/CardTemplate.js";
 import cucharas from "../../../assets/cucharas.js";
 const cucharasBanner = [
-  "/static/media/cucharita 4.4f6c33a1.png",
-  "/static/media/cucharita 7.c020fc69.png",
-  "/static/media/cucharita 10.f6a271a9.png",
+  "/AVRegionales/static/media/cucharita 4.4f6c33a1.png",
+  "/AVRegionales/static/media/cucharita 7.c020fc69.png",
+  "/AVRegionales/static/media/cucharita 10.f6a271a9.png",
 ];
 const pinesBanner = [
-  "/static/media/cucharita 4.4f6c33a1.png",
-  "/static/media/cucharita 7.c020fc69.png",
-  "/static/media/cucharita 10.f6a271a9.png",
+  "/AVRegionales/static/media/cucharita 4.4f6c33a1.png",
+  "/AVRegionales/static/media/cucharita 7.c020fc69.png",
+  "/AVRegionales/static/media/cucharita 10.f6a271a9.png",
 ];
 export default function CardContainer() {
   return (
