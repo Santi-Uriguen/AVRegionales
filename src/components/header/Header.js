f6dd4d36504d74fa683eccd439b637e9f8e3b152
SantiUriguen <santiuriguen@hotmail.com>
2021-04-22 15:51:39 -0300

    styles & scrolling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,6 @@ export default function Header() {
   return (
     <header className="header">
       <HeaderNavbar />
-      <h1 className="title">AV Regionales</h1>
       <HeaderCarousel />
     </header>
   );
--- src/components/header/components/HeaderNavbar.js
+++ src/components/header/components/HeaderNavbar.js
@@ -2,24 +2,37 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Logo from "../../../assets/av.svg";
+import { useEffect, useState } from "react";
 
 export default function HeaderNavbar() {
+  const [className, setClassName] = useState("navBarBig");
   const infoOpen = () => {
     document.getElementById("infoSection").className = "infoShow";
-    document.querySelector("h1").className = "infoHide";
     document.getElementById("cardContainer").className = "infoHide";
     document.getElementById("contact").className = "infoHide";
+    window.scroll({ top: 0, behavior: "smooth" });
   };
   const infoClose = () => {
-    document.querySelector("h1").className = "title";
     document.getElementById("infoSection").className = "infoHide";
     document.getElementById("cardContainer").className = "cardContainer";
     document.getElementById("contact").className = "formSection";
+    window.scroll({ top: 0, behavior: "smooth" });
   };
+  const scrollTo = () => {};
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY >= 100) {
+        setClassName("navBarShort");
+      } else if (window.scrollY < 100) {
+        setClassName("navBarBig");
+      }
+    });
+  });
   return (
-    <Navbar bg="light" expand="lg" className="navBar">
+    <Navbar bg="light" expand="lg" className={"navBar " + className}>
       <Navbar.Brand href="#main" onClick={infoClose}>
         <img src={Logo} alt="AV logo" />
+        <h1 className="title">AV Regionales</h1>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
