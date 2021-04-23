328a9fe4ab65f127d62d4c071982d886468ecc47
SantiUriguen <santiuriguen@hotmail.com>
2021-04-22 23:20:47 -0300

    22/04 last mobile styles

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,17 +7,20 @@ export default function Footer() {
     <footer className="footer">
       <Nav>
         <Card>
-          <Card.Title>Derechos reservados AV Regionales 2021</Card.Title>
           <Card.Footer>
             <div className="link-container">
-              <p>Dirección: </p>
               <Card.Link href="https://goo.gl/maps/J4FKMVQH3Hmu6BbA8">
-                Base Marambio 2203 - Maipú, Mendoza
+                Ubicación:{" "}
+                <span className="spanLink">
+                  Base Marambio al 2000 - Maipú, Mendoza
+                </span>
               </Card.Link>
             </div>
             <div className="link-container">
-              <p>Email:</p>
-              <Card.Link href=""> angeltomasvera@gmail.com</Card.Link>
+              <Card.Link href="">
+                Email:{" "}
+                <span className="spanLink">angeltomasvera@gmail.com</span>
+              </Card.Link>
             </div>
             <div className="link-container">
               <Card.Link
@@ -34,6 +37,7 @@ export default function Footer() {
               </Card.Link>
             </div>
           </Card.Footer>
+          <Card.Title>Derechos reservados AV Regionales 2021</Card.Title>
         </Card>
       </Nav>
     </footer>
